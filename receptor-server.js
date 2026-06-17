f2168646d0765134f439ba90c80d0bfdd96e9b63
AustralCoder <jnicolaskelly.2@gmail.com>
2026-06-17 16:26:24 -0300

    feat: implementar registro asíncrono de eventos, peticiones y errores en la API

--- receptor-server.js
+++ receptor-server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const rutasEscaneo = require('./src/routes/escaneo.routes');
-
+const logger = require('./src/utils/logger');
 const app = express();
 const PUERTO = 3000;
 
 //middlewares
-
 app.use(cors());
 app.use(express.json());
 
 //conexión con el Front-End
 app.use(express.static('public'));
-
 app.use('/api', rutasEscaneo);
 
 
 app.listen(PUERTO, () => {
+    logger.escribirLog('INFO', 'SISTEMA', 'Servidor inicializado con éxito. Bunker central activo');
     console.log(`[Búnker Central]: Escuchando comunicaciones en el puerto ${PUERTO}`);
 })
--- src/controllers/escaneo.controller.js
+++ src/controllers/escaneo.controller.js
@@ -1,15 +1,46 @@
-const iniciarEscaneo = (req, res) => {
-    const urlRecibida = req.body.url;
-    console.log(`[ADUANA]: Objetivo recibido en coordenadas: ${urlRecibida}`);
+const { escribirLog } = require('../utils/logger');
+
+const iniciarEscaneo = async(req, res) => {
+
+    try {
+        const urlRecibida = req.body.url;
+
+        await escribirLog('INFO', 'PETICIÓN', `Ruta Táctica golpeada: POST 'api/escanear'`);
+        await escribirLog('INFO', 'PROCESO', `Objetivo recibido: ${urlRecibida}`);
+
+        /*
+
+        por aca vemos que hacer con el robot cuando esté
+
+        */
+
+        //comunicación con el robot
+
+        await escribirLog('SUCCESS', 'ROBOT', `El Robot escaneó la URL exitosamente`);
 
 
-    /*retorno al front*/
 
-    res.json({
-        estado: '200',
-        mensaje: '[Enlace establecido: Servidor a la espera del robot',
-        objetivo: urlRecibida
-    });
+        /*retorno al front*/
+
+        res.json({
+            estado: '200',
+            mensaje: '[Enlace establecido: Servidor a la espera del robot',
+            objetivo: urlRecibida
+        });
+
+        await escribirLog('INFO', 'RETORNO', `Despachando JSON hacia el Frontend. Estado: 200`)
+    } catch (error) {
+        await escribirLog('ERROR', 'SISTEMA', `Falla crítica en el escaneo ${error.message}`);
+
+        res.status(500).json({
+            estado: 'ERROR',
+            mensaje: 'Falla de comunicación con el escuadrón táctico'
+        });
+
+    }
+
+
+
 };
 
 module.exports = {
